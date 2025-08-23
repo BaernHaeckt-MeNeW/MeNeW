b746051bb5817bb22102546e98e89cfcdec98c3c
Marco Affolter <affom9@bfh.ch>
2025-08-23 07:26:27 +0200

    minor cleanup

--- backend/src/main/java/ch/bernhackt/menew/backend/respository/DietRepository.java
+++ backend/src/main/java/ch/bernhackt/menew/backend/respository/DietRepository.java
@@ -4,8 +4,6 @@ import ch.bernhackt.menew.backend.entity.Diet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
