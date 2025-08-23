7b3e777855a80658bf9570ca7206e02e590a0a0f
Luca Scherer <luca.scherer@students.bfh.ch>
2025-08-23 10:42:10 +0200

    navigation

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,36 +1,31 @@
-import type {ReactNode} from "react";
+import type { ReactNode } from "react";
 
 interface CardProps {
     text?: string;
     icon?: ReactNode;
     onClick?: () => void;
+    interactive?: boolean;
 }
 
-export default function Card({ text, icon, onClick }: CardProps) {
+export default function Card({ text, icon, onClick, interactive }: CardProps) {
     return (
         <div
-            onClick={onClick}
-            className="
+            onClick={interactive ? onClick : undefined}
+            className={`
         bg-gray-800
         text-gray-100
         px-6 py-4
         rounded-2xl
         shadow-md
-        hover:shadow-lg
-        hover:bg-gray-700
-        transition
-        cursor-pointer
         min-w-[200px]
         text-center
-      "
+        transition
+        ${interactive ? "cursor-pointer hover:shadow-lg hover:bg-gray-700 active:scale-95 active:bg-gray-700" : ""}
+      `}
         >
             {text && <span className="text-lg font-medium block">{text}</span>}
 
-            {icon && (
-                <div className="flex justify-center">
-                    {icon}
-                </div>
-            )}
+            {icon && <div className="flex justify-center">{icon}</div>}
         </div>
     );
 }
--- frontend/src/pages/Household.tsx
+++ frontend/src/pages/Household.tsx
@@ -1,17 +1,18 @@
 import {useEffect} from "react";
 import {PersonCard} from "../components/PersonCard.tsx";
 import Card from "../components/Card.tsx";
-import { CirclePlus  } from "lucide-react";
+import {CirclePlus} from "lucide-react";
 import Nav from "../components/Nav.tsx";
+import {Link} from "react-router-dom";
 
 export default function Household() {
 
     //const [persons, setPersons] = useState<Person[]>([]);
 
-    const persons =[
-        { id: 1, name: "Alice", diets: ["Vegetarian"], tags: [] },
-        { id: 2, name: "Bob", diets: ["Vegan"], tags: [] },
-        { id: 3, name: "Charlie", diets: ["Gluten-Free"], tags: [] },
+    const persons = [
+        {id: 1, name: "Alice", diets: ["Vegetarian"], tags: []},
+        {id: 2, name: "Bob", diets: ["Vegan"], tags: []},
+        {id: 3, name: "Charlie", diets: ["Gluten-Free"], tags: []},
     ]
 
     useEffect(() => {
@@ -20,17 +21,19 @@ export default function Household() {
     }, [])
 
     return <>
-        <Nav backButton />
+        <Nav backButton/>
         <div>
             <img src={"/assets/household_img.png"} alt="Household" className="w-60 h-60 rounded-3xl mx-auto mb-4"/>
         </div>
-        <h1 className={"w-full text-center mb-10 mt-5 text-4xl font-extrabold"}>Mein Haushalt</h1>
         <div className="grid grid-cols-1 gap-4 justify-center">
             {persons.map((person, index) => (
                 <PersonCard key={index} name={person.name}/>
             ))}
-            <Card icon={<CirclePlus  className="w-8 h-8"/>} />
+            <Link to="/new-person">
+                <Card icon={<CirclePlus className="w-8 h-8"/>} interactive/>
+            </Link>
+
         </div>
     </>
-    ;
+        ;
 }
