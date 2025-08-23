e15dbf609d7c72c30c436be60100fe941ba13307
Janic Scherer <janic.scherer@students.bfh.ch>
2025-08-23 10:34:04 +0200

    add cool fancy household icon

--- frontend/src/pages/Household.tsx
+++ frontend/src/pages/Household.tsx
@@ -3,7 +3,6 @@ import {PersonCard} from "../components/PersonCard.tsx";
 import Card from "../components/Card.tsx";
 import { CirclePlus  } from "lucide-react";
 import Nav from "../components/Nav.tsx";
-import {Link} from "react-router-dom";
 
 export default function Household() {
 
@@ -22,12 +21,15 @@ export default function Household() {
 
     return <>
         <Nav backButton />
+        <div>
+            <img src={"/assets/household_img.png"} alt="Household" className="w-60 h-60 rounded-3xl mx-auto mb-4"/>
+        </div>
+        <h1 className={"w-full text-center mb-10 mt-5 text-4xl font-extrabold"}>Mein Haushalt</h1>
         <div className="grid grid-cols-1 gap-4 justify-center">
             {persons.map((person, index) => (
                 <PersonCard key={index} name={person.name}/>
             ))}
-            <Link to="/new-person" />
-            <Card icon={<CirclePlus  className="w-8 h-8" />} />
+            <Card icon={<CirclePlus  className="w-8 h-8"/>} />
         </div>
     </>
     ;
