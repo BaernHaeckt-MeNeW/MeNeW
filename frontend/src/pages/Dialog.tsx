c21bb3f8c551ec36f412d8b618ac901839942f41
Janic Scherer <janic.scherer@students.bfh.ch>
2025-08-24 10:57:44 +0200

    use transparent bar on dialog view

--- frontend/src/pages/Dialog.tsx
+++ frontend/src/pages/Dialog.tsx
@@ -224,7 +224,7 @@ export default function Dialog() {
 
     return (
         <div className="h-screen overflow-hidden">
-            <div className="fixed top-0 left-0 right-0 z-50 h-16 py-6 px-4 bg-background">
+            <div className="fixed top-0 left-0 right-0 z-50 h-16 py-6 px-4 bg-transparent">
                 <Nav
                     backButton
                     topCenterElement={
